029b061f71c44f8d2c1ce986f13fb05ed8792652
huynhviethung214 <44541026+huynhviethung214@users.noreply.github.com>
2019-07-14 21:15:57 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -10,7 +10,8 @@ from kivy.uix.treeview import TreeViewLabel, TreeView
 from kivy.lang import Builder
 from kivy.app import App
 from functools import partial
-from node import Node
+from kivy.clock import Clock
+from node import Node, NodeLink
 
 
 class Component(TreeViewLabel):
@@ -19,7 +20,7 @@ class Component(TreeViewLabel):
 		self._attachment = _attachment
 		self.text = _name
 
-	def on_touch_move(self, touch):
+	def on_touch_down(self, touch):
 		if self.collide_point(*touch.pos):
 			Interface._node = self._attachment
 			Interface._state = 1
@@ -38,14 +39,34 @@ class ToolBar(BoxLayout, Widget):
 	pass
 
 
-class Interface(BoxLayout, StencilView, Widget):
+class Interface(StencilView, Widget):
 	_node = None
 	_state = 0
+	_spos = (0, 0)
 
 	def __init__(self, **kwargs):
 		super(Interface, self).__init__()
+		# self._origin = (0, 0)
+
 		self.bind(on_touch_move=self.is_validate)
 		self.bind(on_touch_up=partial(self._add_node, _self=self))
+		self.bind(on_touch_up=partial(self._connect, _type=1))
+		self.bind(on_touch_down=partial(self._connect, _type=0))
+
+	def _connect(self, obj, touch, _type):
+		for node in self.children[0].children:
+			for node_link in node.children[0].children:
+				if type(node_link) == NodeLink:
+					pos = node_link.to_widget(*touch.pos)
+					if node_link.collide_point(*pos):
+						if _type == 1:
+							node._bind(_self=node,
+									   state=2,
+									   nav=node_link._type)
+						elif _type == 0:
+							node._bind(_self=node,
+									   nav=node_link._type)
+						return True
 
 	def is_validate(self, obj, touch):
 		if self.collide_point(*touch.pos):
@@ -56,17 +77,26 @@ class Interface(BoxLayout, StencilView, Widget):
 
 	@classmethod
 	def _add_node(cls, obj, touch, _self):
-		if cls._state == 1 and _self.collide_point(*touch.pos):
-			pos = _self.children[0].to_local(*touch.pos)
-			node = cls._node(spos=pos)
+		if _self.collide_point(*touch.pos):
+			if cls._state == 1:
+				pos = _self.children[0].to_local(*touch.pos)
+				node = cls._node(spos=pos)
 
-			_self.children[0].add_widget(node)
-			cls._node = None
-			cls._state = 0
-		else:
-			cls._node = None
-			cls._state = 0
+				_self.children[0].add_widget(node)
+				cls._state = 0
+				cls._node = None
 		return True
+
+	def _set_origin(self, obj, touch):
+		for node in self.children[0].children:
+			for _node_link in node.children[0].children:
+				if type(_node_link) == NodeLink:
+					pos = _node_link.to_widget(*touch.pos)
+					if _node_link.collide_point(*pos):
+						self._origin = self.children[0].to_widget(*pos)
+
+	def _draw_link(self, obj, touch):
+		pass
 
 
 class SubContainer(BoxLayout, Widget):
