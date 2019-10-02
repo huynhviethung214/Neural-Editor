6270076f7050ab0347fd3152b10ae0a261e100eb
huynhviethung214 <44541026+huynhviethung214@users.noreply.github.com>
2019-10-02 16:43:17 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,8 +3,6 @@ from torch.nn import Module, Sequential, ModuleList
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
-# FIX THE SORTING ALGORITHM
 class Sorter(object):
 	temp_model = []
 	train_set = None
@@ -13,25 +11,14 @@ class Sorter(object):
 
 	@staticmethod
 	def setup(layers):
-		# print(layers)
-		# print('====')
-
 		for layer in layers:
 			for node in layer:
 				if node._type == 0:
 					Sorter.temp_model.insert(0, layer[0])
-					# Sorter.temp_model.insert(1, layer[1])
 					Sorter.indicator = layer[1]
-					# layers.remove(layer)
 
 				elif node._type == 2:
 					Sorter.temp_model.insert(1, layer[1])
-					# Sorter.temp_model.insert(len(Sorter.temp_model)-1, layer[0])
-					# layers.remove(layer)
-
-				# elif node._type == 3:
-				# 	Sorter.train_set, Sorter.test_set = node.processor._dataset()
-		# print(Sorter.temp_model)
 
 	@staticmethod
 	def _model(layers):
@@ -40,21 +27,9 @@ class Sorter(object):
 				Net.model.append(layer)
 		return Net().to(device)
 
-	# THE PROBLEM IS RIGHT BELOW
 	def sort(self, layers):
-		# print(layers)
-		# print('====')
-		# print(Sorter.temp_model)
-
-		# c_ind = Sorter.temp_model[0]
 		c_ind = Sorter.indicator
 		ci = 1
-
-		# for layer in layers:
-		# 	for _layer in layers:
-		# 		if layer[1] == _layer[0]:
-		# 			Sorter.temp_model.insert(ci, layer[1].algorithm())
-		# 			ci += 1
 
 		# SORTING ALGORITHM
 		for layer in layers[1:]:
@@ -63,24 +38,11 @@ class Sorter(object):
 				ci += 1
 				c_ind = layer[1]
 
-		# print(Sorter.temp_model)
-		# print('===============')
-
-		# MERGING ALGORITHM
-		# temp_model = []
-		# for layer in Sorter.temp_model:
-		# 	temp_model = list(set(temp_model + layer))
-
-		# print(temp_model)
-		# print('====')
-
 		#EXTRACTING LAYER OUT OF AN ARRAY OF LAYERS
 		model = []
 		for block in Sorter.temp_model:
 			model.append(block.algorithm())
-		# print(model)
 
-		# return Sequential(*Sorter.temp_model), (Sorter.train_set, Sorter.test_set)
 		return self._model(model)
 
 
